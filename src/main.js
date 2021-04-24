4bbf83fe63f96faeae6d5c4f9a43101b3d6acdcd
fabio98p <parisifabio98@gmail.com>
2021-04-24 21:01:49 +0200

    remote training cambiato nome fail per renderli piu chiari crezione partiton card messo js in src

---
+++ src/main.js
@@ -0,0 +1,23 @@
+Vue.config.devtools = true;
+
+document.addEventListener('DOMContentLoaded', function () {
+	console.log('ciao ale');
+	var root = new Vue(
+		{
+			el: '#root',
+			data: {
+				hello: "ciao",
+			},
+
+			methods: {
+
+			},
+			compiuted:{
+				
+			},
+
+			created: function () {
+			}
+		}
+	);
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 let mix = require('laravel-mix');
 
+mix.js('src/main.js', 'assets/js/');
+
 mix.sass('src/master.scss', 'assets/css/').options({
     processCssUrls: false
 });
