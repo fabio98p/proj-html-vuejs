3c1c02021cf59791bb58ca12fbaf514f42ca5538
fabio98p <parisifabio98@gmail.com>
2021-04-26 00:03:18 +0200

    remote training creazione cartella data.js con all'interno tutti i dadi necessari

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,14 @@
 let mix = require('laravel-mix');
 
-mix.js('src/main.js', 'assets/js/');
 
 mix.sass('src/master.scss', 'assets/css/').options({
     processCssUrls: false
 });
+
+mix.js('src/main.js', 'assets/js/');
+// require('laravel-mix-js-partial');
+
+// mix.jsPartial([
+//     'src/partial_js/_data.js',
+//     'src/main.js'
+// ], 'assets/js/main.js');
