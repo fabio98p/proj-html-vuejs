94fee3cf49b2faa164240a668de893bf515bd784
fabio98p <parisifabio98@gmail.com>
2021-04-26 14:35:26 +0200

    remote training inserito tutti i dati nell'index con vue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
 Vue.config.devtools = true;
+import similAxsios from "./partial_js/_data.js"
+import cardNumber from "./partial_js/_data.js"
+import cardService from "./partial_js/_data.js"
+import ulService from "./partial_js/_data.js"
+import cardCourses from "./partial_js/_data.js"
+import cardAbout from "./partial_js/_data.js"
+import logosAbout from "./partial_js/_data.js"
+import cardBlog from "./partial_js/_data.js"
+import cardHelp from "./partial_js/_data.js"
+import footerInfo from "./partial_js/_data.js"
 document.addEventListener('DOMContentLoaded', function () {
 	console.log('ciao ale');
 	var root = new Vue(
 		{
 			el: '#root',
 			data: {
-				hello: "ciao",
+				navbarSelect: similAxsios.navbarSelect,
+				cardNumber: similAxsios.cardNumber,
+				cardService: similAxsios.cardService,
+				ulService: similAxsios.ulService,
+				cardCourses: similAxsios.cardCourses,
+				cardAbout: similAxsios.cardAbout,
+				logosAbout: similAxsios.logosAbout,
+				cardBlog: similAxsios.cardBlog,
+				cardHelp: similAxsios.cardHelp,
+				logosSocial: similAxsios.logosSocial,
+				footerInfo: similAxsios.footerInfo,
 			},
 
 			methods: {
@@ -16,6 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
 			},
 
 			created: function () {
+				console.log(this.cardAbout[1].img);
 			}
 		}
 	);
--- src/partial_js/_data.js
+++ src/partial_js/_data.js
@@ -1,209 +1,218 @@
-const navbarSelect = [
-	{nameOption: "Home"},
-	{nameOption: "Pages"},
-	{nameOption: "Courses"},
-	{nameOption: "Features"},
-	{nameOption: "Blog"},
-	{nameOption: "Shop"},
-]
-
-const cardNumber = [
-	{
-		number: "1.926",
-		aboutNumber: "Finished session",
-	},
-	{
-		number: "3.092",
-		aboutNumber: "Enrolled learners",
-	},
-	{
-		number: "200",
-		aboutNumber: "Online istructor",
-	},
-	{
-		number: "100%",
-		aboutNumber: "Satisfaction rate",
-	},
-]
-const cardService = [
-	{
-		title: "Graphic Design",
-		paragraph: "Have a passion for graphics and arts? Show your talents with confidence and self-assertiveness.",
-		img: "01",
-		imgPosition: "down",
-	},
-	{
-		title: "Idea Discussion",
-		paragraph: "Get teamed up with the specialists who work and teach coding for years at famous universities.",
-		img: "02",
-		imgPosition: "up",
-	},
-	{
-		title: "Business Administration",
-		paragraph: "Learners are encouraged to study the mechanism and structure of system administration.",
-		img: "03",
-		imgPosition: "down",
-	},
-	{
-		title: "Web Development",
-		paragraph: "Learn to start building a webpage from scratch. You decide your own pace, course and speed.",
-		img: "04",
-		imgPosition: "up",
-	},
-]
-
-const ulService = [
-	{nameLi: "Select & customize courses to your preferences"},
-	{nameLi: "Change the tutor and make arrangements"},
-	{nameLi: "Participate in events to join others"},
-	{nameLi: "Get the desired certificate delivered at house"},
-]
-
-const cardCourses = [
-	{
-		img: "01",
-		cost: "40.00",
-		paragraph: "Learning to writhe as a Professional Author",
-		lesson: "20",
-		student: "50",
-	},
-	{
-		img: "02",
-		cost: "Free",
-		paragraph: "Customer-cintric Info-Tech Strategies",
-		lesson: "24",
-		student: "769",
-	},
-	{
-		img: "03",
-		cost: "$19.00",
-		paragraph: "Open Programming Courses for Evryone: Python",
-		lesson: "17",
-		student: "62",
-	},
-	{
-		img: "04",
-		cost: "$26.00",
-		paragraph: "Academic Listening and Note-taking",
-		lesson: "14",
-		student: "67",
-	},
-	{
-		img: "05",
-		cost: "$39.00",
-		paragraph: "Master jQuery in a Shotr Period Time",
-		lesson: "6",
-		student: "51",
-	},
-	{
-		img: "06",
-		cost: "$59.00",
-		paragraph: "Itroduction to Javascript for Beginners",
-		lesson: "14",
-		student: "76",
-	},
-]
-
-const card_about = [
-	{
-		img: "01",
-		paragraph: "I am free to learn at my own pace, follow my own schedule and choose the subject I like. Great study portal for people like me.",
-		author: "Mina Hollace",
-		work: "/Freelancer",
-	},
-	{
-		img: "02",
-		paragraph: "MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.",
-		author: "Luvic Dubble",
-		work: "/Private Tutor",
-	},	
-	{
-		img: "03",
-		paragraph: "MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.",
-		author: "Mina Hollace",
-		work: "/Freelancer",
-	},	
-	{
-		img: "04",
-		paragraph: "I'm a very strict person so I require everything to be organized and neat. MaxCoach guys just got me.",
-		author: "Florence Themes",
-		work: "/Multimedia Admin",
-	},
-]
-
-const logosAbout = [
-	{logo: "01"},
-	{logo: "02"},
-	{logo: "03"},
-	{logo: "04"},
-	{logo: "05"},
-	{logo: "06"},
-]
-
-const cardBlog = [
-	{
-		img: "01",
-		work: "Artist",
-		paragraph: "Brush Strokes Energize Trees in Painting",
-		data: "May 15, 2020",
-		views: "688",
-	},
-	{
-		img: "02",
-		work: "Artist",
-		paragraph: "Poket-Sized Notebooks Hold Miniature Paintings",
-		data: "May 15, 2020",
-		views: "603",
-	},
-	{
-		img: "03",
-		work: "Artist",
-		paragraph: "Connection between Self-Protrains and Identity",
-		data: "May 15, 2020",
-		views: "397",
-	},
-]
-
-const cardBlog = [
-	{
-		img: "01",
-		title: "Idea Discussion",
-		paragraph: "Get teamed up with the specialists who work and teach coding for years at famous universities.",
-	},
-	{
-		img: "02",
-		title: "Web Development",
-		paragraph: "Learn to start building a webpage from scratch. You decide your own pace, course and speed.",
-	},
-	{
-		img: "03",
-		title: "System Administration",
-		paragraph: "Learners are encouraged to study the mechanism and structure of system administration.",
-	},
-	{
-		img: "04",
-		title: "Graphic Design",
-		paragraph: "Have a passion for graphics and arts? Show your talents with confidence and self-assertiveness.",
-	},
-]
-
-const footerInfo = [
-	explore = [
-		{text: "Start Here"},
-		{text: "Success story"},
-		{text: "Blog"},
-		{text: "Courses"},
-		{text: "About us"},
-		{text: "Contact us"},
+const similAxsios = {
+	navbarSelect: [
+		{ nameOption: "Home" },
+		{ nameOption: "Pages" },
+		{ nameOption: "Courses" },
+		{ nameOption: "Features" },
+		{ nameOption: "Blog" },
+		{ nameOption: "Shop" },
 	],
-	information = [
-		{text: "Membership"},
-		{text: "Purchase guide"},
-		{text: "Privacy policy"},
-		{text: "Terms of service"},
-	]
-]
 
+	cardNumber: [
+		{
+			number: "1.926",
+			aboutNumber: "Finished session",
+		},
+		{
+			number: "3.092",
+			aboutNumber: "Enrolled learners",
+		},
+		{
+			number: "200",
+			aboutNumber: "Online istructor",
+		},
+		{
+			number: "100%",
+			aboutNumber: "Satisfaction rate",
+		},
+	],
+	
+	cardService: [
+		{
+			title: "Graphic Design",
+			paragraph: "Have a passion for graphics and arts? Show your talents with confidence and self-assertiveness.",
+			img: "01",
+			imgPosition: "down",
+		},
+		{
+			title: "Idea Discussion",
+			paragraph: "Get teamed up with the specialists who work and teach coding for years at famous universities.",
+			img: "03",
+			imgPosition: "up",
+		},
+		{
+			title: "Business Administration",
+			paragraph: "Learners are encouraged to study the mechanism and structure of system administration.",
+			img: "02",
+			imgPosition: "down",
+		},
+		{
+			title: "Web Development",
+			paragraph: "Learn to start building a webpage from scratch. You decide your own pace, course and speed.",
+			img: "04",
+			imgPosition: "up",
+		},
+	],
+	
+	ulService: [
+		{ nameLi: "Select & customize courses to your preferences" },
+		{ nameLi: "Change the tutor and make arrangements" },
+		{ nameLi: "Participate in events to join others" },
+		{ nameLi: "Get the desired certificate delivered at house" },
+	],
+	
+	cardCourses: [
+		{
+			img: "01",
+			cost: "40.00",
+			paragraph: "Learning to writhe as a Professional Author",
+			lesson: "20",
+			student: "50",
+		},
+		{
+			img: "02",
+			cost: "Free",
+			paragraph: "Customer-cintric Info-Tech Strategies",
+			lesson: "24",
+			student: "769",
+		},
+		{
+			img: "03",
+			cost: "$19.00",
+			paragraph: "Open Programming Courses for Evryone: Python",
+			lesson: "17",
+			student: "62",
+		},
+		{
+			img: "04",
+			cost: "$26.00",
+			paragraph: "Academic Listening and Note-taking",
+			lesson: "14",
+			student: "67",
+		},
+		{
+			img: "05",
+			cost: "$39.00",
+			paragraph: "Master jQuery in a Shotr Period Time",
+			lesson: "6",
+			student: "51",
+		},
+		{
+			img: "06",
+			cost: "$59.00",
+			paragraph: "Itroduction to Javascript for Beginners",
+			lesson: "14",
+			student: "76",
+		},
+	],
+	
+	cardAbout: [
+		{
+			img: "01",
+			paragraph: "I am free to learn at my own pace, follow my own schedule and choose the subject I like. Great study portal for people like me.",
+			author: "Mina Hollace",
+			work: "/Freelancer",
+		},
+		{
+			img: "02",
+			paragraph: "MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.",
+			author: "Luvic Dubble",
+			work: "/Private Tutor",
+		},
+		{
+			img: "03",
+			paragraph: "MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.",
+			author: "Mina Hollace",
+			work: "/Freelancer",
+		},
+		{
+			img: "04",
+			paragraph: "I'm a very strict person so I require everything to be organized and neat. MaxCoach guys just got me.",
+			author: "Florence Themes",
+			work: "/Multimedia Admin",
+		},
+	],
+	
+	logosAbout: [
+		{ logo: "01" },
+		{ logo: "02" },
+		{ logo: "03" },
+		{ logo: "04" },
+		{ logo: "05" },
+		{ logo: "06" },
+	],
+	
+	cardBlog: [
+		{
+			img: "01",
+			work: "Artist",
+			paragraph: "Brush Strokes Energize Trees in Painting",
+			data: "May 15, 2020",
+			views: "688",
+		},
+		{
+			img: "02",
+			work: "Artist",
+			paragraph: "Poket-Sized Notebooks Hold Miniature Paintings",
+			data: "May 15, 2020",
+			views: "603",
+		},
+		{
+			img: "03",
+			work: "Artist",
+			paragraph: "Connection between Self-Protrains and Identity",
+			data: "May 15, 2020",
+			views: "397",
+		},
+	],
+	
+	cardHelp: [
+		{
+			icon: "far fa-flag",
+			title: "Idea Discussion",
+			paragraph: "Get teamed up with the specialists who work and teach coding for years at famous universities.",
+		},
+		{
+			icon: "fas fa-cog",
+			title: "Web Development",
+			paragraph: "Learn to start building a webpage from scratch. You decide your own pace, course and speed.",
+		},
+		{
+			icon: "fas fa-life-ring",
+			title: "System Administration",
+			paragraph: "Learners are encouraged to study the mechanism and structure of system administration.",
+		},
+		{
+			icon: "fas fa-tv",
+			title: "Graphic Design",
+			paragraph: "Have a passion for graphics and arts? Show your talents with confidence and self-assertiveness.",
+		},
+	],
 
+	logosSocial: [
+		{ icon: "fab fa-facebook-square"},
+		{ icon: "fab fa-twitter"},
+		{ icon: "fab fa-instagram"},
+		{ icon: "fab fa-linkedin"},
 
+	],
+	
+	footerInfo: {
+		explore: [
+			{ text: "Start Here" },
+			{ text: "Success story" },
+			{ text: "Blog" },
+			{ text: "Courses" },
+			{ text: "About us" },
+			{ text: "Contact us" },
+		],
+		information: [
+			{ text: "Membership" },
+			{ text: "Purchase guide" },
+			{ text: "Privacy policy" },
+			{ text: "Terms of service" },
+		]
+	}
+}
 
+export default similAxsios
